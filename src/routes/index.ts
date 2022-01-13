61202fb5e14233342d33dbd850915e0e8ac09852
mahdij <mahdimn2011@yahoo.com>
2022-01-13 09:29:43 +0330

    user handle post request added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,27 @@
 import app from 'express'
+import statusCodes from '../utils/status-codes'
+import { user } from '../models/user'
+import userService from '../services/userService'
+import Errors from '../utils/errors'
+import errorMsgs from '../utils/error-messages/error-english'
 
 const router = app.Router()
+
+router.post('/users', (req, res) => {
+    const user: user = req.body
+    if (!user.email || !user.mobile || !user.username) {
+        res.status(statusCodes.BAD_REQUEST).send(Errors.BadRequestError())
+        return
+    }
+    if (user.password.length < 6) {
+        res.status(statusCodes.FORBIDDEN).send(Errors.ForbiddenError(errorMsgs.password_short()))
+        return
+    }
+    userService.save(user).then(() => {
+        res.status(statusCodes.SUCCESSFUL).send({ username: user.username })
+    }).catch(e => {
+        res.status(e.status).send(e)
+    })
+})
 
 export default router
