2044cd4767f18523087cd823ed7ef77a3c01d8a6
mahdij <mahdimn2011@yahoo.com>
2022-02-28 09:35:20 +0330

    port fixed

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import config from "../config/config";
 
 const sequelize = new Sequelize(config.DB_DB, config.DB_USER, config.DB_PASS, {
     host: config.DB_HOST,
-    // port: Number(config.DB_PORT),
+    port: Number(config.DB_PORT),
     dialect: 'postgres',
     logging: process.env.MODE === 'dev'
   })
